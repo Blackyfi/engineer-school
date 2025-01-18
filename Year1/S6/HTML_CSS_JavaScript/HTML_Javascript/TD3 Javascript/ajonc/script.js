b7c1379f5e7a212addd48f54eab6f579ce5a013b
nicolas <nicolas.thongphao@gmail.com>
2025-01-18 20:58:29 +0100

    Add initial HTML, CSS, and JavaScript files for Semester 6 projects

--- Year1/S6/HTML_CSS_JavaScript/HTML_Javascript/TD3 Javascript/ajonc/script.js
+++ Year1/S6/HTML_CSS_JavaScript/HTML_Javascript/TD3 Javascript/ajonc/script.js
@@ -1,9 +1,43 @@
-function changeWidth(x,y) {
-    if (typeof x == 'img' && typeof y == 'int') {
-        x.style.width = y+"px";
-    } else {
-        alert("nope sike");
+function changeWidth(imageElement, width) {
+    if (imageElement instanceof HTMLImageElement) {
+        imageElement.style.width = `${width}px`;
     }
+}
 
-    return;
-};
+
+function loupe(element) {
+    const computedStyle = window.getComputedStyle(element);
+    const currentFontSize = parseFloat(computedStyle.fontSize);
+    element.style.fontSize = `${currentFontSize * 2}px`;
+}
+
+
+
+
+
+function tousVerts(tagName, root = document) {
+    const elements = root.getElementsByTagName(tagName);
+    for (let element of elements) {
+        element.style.color = "green";
+    }
+}
+
+
+
+const droiteElements = document.getElementsByClassName('droite');
+for (let element of droiteElements) {
+    element.style.padding = "20px";
+}
+
+const ajoncs = document.getElementById('ajoncs');
+const parElements = ajoncs.getElementsByClassName('par');
+for (let element of parElements) {
+    element.style.border = "1px solid blue";
+}
+
+function cache(className) {
+    const elements = document.getElementsByClassName(className);
+    for (let element of elements) {
+        element.style.display = "none";
+    }
+}
